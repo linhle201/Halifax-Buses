4ec09191f40d5c4bda21d4ea877b56105eae05c3
linhle201 <144468241+linhle201@users.noreply.github.com>
2024-02-24 16:55:19 +0000

    save some changes

--- buses/main.js
+++ buses/main.js
@@ -8,9 +8,9 @@
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-    L.marker([44.650690, -63.596537]).addTo(map)
-        .bindPopup('This is a sample popup. You can put any html structure in this including extra bus data. You can also swap this icon out for a custom icon. A png file has been provided for you to use if you wish.')
-        .openPopup();
+    //L.marker([44.650690, -63.596537]).addTo(map)
+        //.bindPopup('This is a sample popup. You can put any html structure in this including extra bus data. You can also swap this icon out for a custom icon. A png file has been provided for you to use if you wish.')
+        //.openPopup();
 
 
      /* Author: Linh Le - W0476946.
@@ -26,8 +26,9 @@
         .then(response => response.json())
         .then(json => {
             console.log(json);
-            convertIntoGeoJsonFormat(json);//Convert Raw Data into GeoJSON format
+            let data= convertIntoGeoJsonFormat(json);//Convert Raw Data into GeoJSON format
             console.log(convertIntoGeoJsonFormat(json));
+            markerOnMap(data);
         })
     
     
@@ -53,7 +54,23 @@
     }
 
     //REQ-3: Plot Markers on Map to Show Position of each Vehicle
-    
+    function markerOnMap(data){
+       
+        data.features.forEach(feature => {
+        L.marker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]])
+        .addTo(map)
+        .bindPopup(`<p>Vehicle ID:</p> ${feature.properties.label}<br>
+                    <p>Trip ID:</p> ${feature.properties.trip}<br>
+                    <p>Route:</p> ${feature.properties.route}<br>
+                    <p>Timestamp:</p> ${feature.properties.timestamp}<br>
+                    <p>Speed:</p> ${feature.properties.speed}`)
+        .openPopup();
+        })
+    }
+
+    //REQ-4: Add Auto-Refresh Functionality to the Page
+    setInterval(markerOnMap, 5000);
+    //REQ-5: Additional Functionality
 
 
 })()
